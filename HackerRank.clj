d041d7825690df856ed99c095bf25ae10454488e
Arthur Henrique <arthurhenrique@users.noreply.github.com>
2018-04-15 22:05:09 -0300

    Update HackerRank.clj

--- HackerRank.clj
+++ HackerRank.clj
@@ -1,18 +1,18 @@
 ;Hello World N Times
 (defn hello_word_n_times[n]
-    ;break case
-    (cond
-     (> n 1) (hello_word_n_times (- n 1) ))
+  ;break case
+  (cond
+    (> n 1) (hello_word_n_times (- n 1) ))
     ;code
-    (print "Hello World")
-)
-;Repeat n itens of array
+    (print "Hello World"))
+
+
+;List Repeats
 (defn g [length item]
-	(if (> length 0)
+  (if (> length 0)
 		(println item))
 	(if (> length 1)
 		(g (- length 1) item)))
-
 ;unit test
 (g 2 2)
 
@@ -21,6 +21,5 @@
 		(g length (peek itens)))
 	(if (> (count itens) 0)
 		(f length (pop itens))))
-
 ;unit test
 (f 2 '(1 2 3))
